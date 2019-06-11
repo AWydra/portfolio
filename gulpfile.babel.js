af443e6ed18115792888db39fa9a54f746014a54
Arkadiusz Wydra <hello@awydra.pl>
2019-06-11 22:22:34 +0200

    Added plugin for tinify png

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,89 +1,110 @@
-import gulp from 'gulp';
-import babel from 'gulp-babel';
-import sass from 'gulp-sass';
-import uglify from 'gulp-uglify';
-import concat from 'gulp-concat';
-import autoprefixer from 'gulp-autoprefixer';
-import clean from 'gulp-clean-css';
-import browserSync from 'browser-sync';
-import del from 'del';
+import gulp from "gulp";
+import babel from "gulp-babel";
+import sass from "gulp-sass";
+import uglify from "gulp-uglify";
+import concat from "gulp-concat";
+import autoprefixer from "gulp-autoprefixer";
+import clean from "gulp-clean-css";
+import browserSync from "browser-sync";
+import del from "del";
+import tinypng from "gulp-tinypng-unlimited";
 
 const sync = browserSync.create();
 const reload = sync.reload;
 const config = {
-    paths: {
-        src: {
-            html: './src/**/*.html',
-            img: './src/img/**/*.*',
-            sass: 'src/sass/style.scss',
-            js: 'src/js/**/*.js'
-        },
-        dist: {
-            main: './dist',
-            css: './dist/css',
-            js: './dist/js',
-            img: './dist/img'
-        }
+  paths: {
+    src: {
+      html: "./src/**/*.html",
+      img: "./src/img/**/*.*",
+      sass: "src/sass/style.scss",
+      js: "src/js/**/*.js"
+    },
+    dist: {
+      main: "./dist",
+      css: "./dist/css",
+      js: "./dist/js",
+      img: "./dist/img"
     }
+  }
 };
 
-gulp.task('sass', () => {
-    return gulp.src(config.paths.src.sass)
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            grid: true
-        }))
-        .pipe(clean())
-        .pipe(gulp.dest(config.paths.dist.css))
-        .pipe(sync.stream());
+gulp.task("sass", () => {
+  return gulp
+    .src(config.paths.src.sass)
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        grid: true
+      })
+    )
+    .pipe(clean())
+    .pipe(gulp.dest(config.paths.dist.css))
+    .pipe(sync.stream());
 });
 
 function refresh(done) {
-    reload();
-    done();
+  reload();
+  done();
 }
 
-gulp.task('js', gulp.series(function js() {
-    return gulp.src(config.paths.src.js)
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest(config.paths.dist.js));
-}, refresh));
+gulp.task(
+  "js",
+  gulp.series(function js() {
+    return gulp
+      .src(config.paths.src.js)
+      .pipe(
+        babel({
+          presets: ["env"]
+        })
+      )
+      .pipe(concat("main.js"))
+      .pipe(uglify())
+      .pipe(gulp.dest(config.paths.dist.js));
+  }, refresh)
+);
 
-gulp.task('static', gulp.series(
-    function moveHtml() {
-        return gulp.src(config.paths.src.html)
-            .pipe(gulp.dest(config.paths.dist.main));
-    },
-    function moveImages() {
-        return gulp.src(config.paths.src.img)
-            .pipe(gulp.dest(config.paths.dist.img));
-    },
-    refresh
-));
+gulp.task(
+  "static",
+  gulp.series(function moveHtml() {
+    return gulp
+      .src(config.paths.src.html)
+      .pipe(gulp.dest(config.paths.dist.main));
+  }, refresh)
+);
 
-gulp.task('clean', () => {
-    return del([config.paths.dist.main]);
+gulp.task(
+  "images",
+  gulp.series(function moveImages() {
+    return gulp
+      .src(config.paths.src.img)
+      .pipe(tinypng())
+      .pipe(gulp.dest(config.paths.dist.img));
+  }, refresh)
+);
+
+gulp.task("clean", () => {
+  return del([config.paths.dist.main]);
 });
 
-gulp.task('build', gulp.series(['clean', 'sass', 'js', 'static']));
+gulp.task("build", gulp.series(["clean", "sass", "js", "static", "images"]));
 
 function server() {
-    sync.init({
-        injectChanges: true,
-        server: config.paths.dist.main
-    });
-};
+  sync.init({
+    injectChanges: true,
+    server: config.paths.dist.main
+  });
+}
 
-gulp.task('default', gulp.series(['build']));
+gulp.task("default", gulp.series(["build"]));
 
-gulp.task('watch', gulp.series(['default'], function watch() {
-    gulp.watch('src/sass/**/*.scss', gulp.series(['sass']));
-    gulp.watch('src/js/**/*.js', gulp.series(['js']));
-    gulp.watch('src/*.html', gulp.series(['static']));
+gulp.task(
+  "watch",
+  gulp.series(["default"], function watch() {
+    gulp.watch("src/sass/**/*.scss", gulp.series(["sass"]));
+    gulp.watch(config.paths.src.js, gulp.series(["js"]));
+    gulp.watch(config.paths.src.html, gulp.series(["static"]));
+    gulp.watch(config.paths.src.img, gulp.series(["images"]));
     return server();
-}));
+  })
+);
