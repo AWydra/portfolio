46926b46fc34e06dfdab7c62ad68e4b2fe0abe3c
Arkadiusz Wydra <hello@awydra.pl>
2019-07-15 23:56:36 +0200

    Router works fine on web site and localhost

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@ $illegalLimit = 3;
 $explodedValue = 3;
 $illegal = false;
 
-if(strpos($_SERVER["SERVER_NAME"], '127.0.0.1') !== false) {
+if(strpos($_SERVER["SERVER_NAME"], '127.0.0.1') !== false) { //If site works on localhost
   $_SERVER["SERVER_NAME"] = 'localhost:3000/portfolio/dist';
   $illegalLimit = 5;
   $explodedValue = 3;
@@ -16,11 +16,18 @@ if(sizeof($urlExploded) > $illegalLimit) {
   $illegal = true;
 }
 
-if ($illegal) {
+
+if ($illegal) { //If slug is incorrect
   $page = '404.php';
   include 'layout.php';
   exit;
+} else if (sizeof($urlExploded) === $illegalLimit - 1){ //If there is no slug
+  $page = 'home.php';
+  include 'layout.php';
+  exit;
 }
+
+//Connect with DB here;
 
 $page = 'project.php';
 include 'layout.php';
