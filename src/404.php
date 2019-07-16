2144aa1b70a571cd1673bb173cc69e487c4c6f47
Arkadiusz Wydra <hello@awydra.pl>
2019-07-16 23:53:19 +0200

    Created structure for 404  page

--- src/404.php
+++ src/404.php
@@ -1,1 +1,20 @@
-404
+<main class="error404">
+  <header class="title">
+    <div class="title__holder">
+      <h2 class="title__holder--heading">A co tu się dzieje??</h2>
+      <p class="title__holder--subtitle">
+        404 Strona nie znaleziona
+      </p>
+    </div>
+  </header>
+  <article class="error404__content">
+    <p class="error404__text">
+      Albo majsterkowałeś przy adresie w pasku adresowym przeglądarki, albo
+      właśnie moja baza danych rozsypała się w drobny mak...
+    </p>
+    <p class="error404__text">Lepiej już wróć do strony głównej i zacznij od początku ;)</p>
+    <a href="//<?= $_SERVER['SERVER_NAME'] ?>" class="btn btn--theme btn--medium">
+      Strona główna
+    </a>
+  </article>
+</main>
