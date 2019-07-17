17682bc46e944020cc3dc48ad5a3acc07c4e75bc
AWydra <hello@awydra.pl>
2019-07-17 18:16:39 +0200

    404 page is done

--- src/404.php
+++ src/404.php
@@ -1,20 +1,22 @@
 <main class="error404">
-  <header class="title">
-    <div class="title__holder">
-      <h2 class="title__holder--heading">A co tu się dzieje??</h2>
-      <p class="title__holder--subtitle">
-        404 Strona nie znaleziona
+  <section class="error404__container">
+    <header class="title title--error404">
+      <div class="title__holder">
+        <h1 class="title__holder--heading">A co tu się dzieje??</h1>
+        <p class="title__holder--subtitle">
+          404 Strona nie znaleziona
+        </p>
+      </div>
+    </header>
+    <article class="error404__content">
+      <p class="error404__text">
+        Albo majsterkowałeś przy adresie w pasku adresowym przeglądarki, albo
+        właśnie moja baza danych rozsypała się w drobny mak...
       </p>
-    </div>
-  </header>
-  <article class="error404__content">
-    <p class="error404__text">
-      Albo majsterkowałeś przy adresie w pasku adresowym przeglądarki, albo
-      właśnie moja baza danych rozsypała się w drobny mak...
-    </p>
-    <p class="error404__text">Lepiej już wróć do strony głównej i zacznij od początku ;)</p>
-    <a href="//<?= $_SERVER['SERVER_NAME'] ?>" class="btn btn--theme btn--medium">
-      Strona główna
-    </a>
-  </article>
+      <p class="error404__text">Lepiej już wróć do strony głównej i zacznij od początku ;)</p>
+      <a href="//<?= $_SERVER['SERVER_NAME'] ?>" class="btn btn--theme btn--medium">
+        Strona główna
+      </a>
+    </article>
+  </section>
 </main>
