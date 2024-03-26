cb2410d0c354ec4f47067d2ac876c019cb025fbe
AWydra <hello@awydra.pl>
2024-03-26 19:04:02 +0100

    changed project structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "#041230",
+        secondary: "#ff7800",
       },
     },
   },
