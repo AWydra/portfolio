c834bec4936a563dfa7a404870fbf70712542e9a
AWydra <hello@awydra.pl>
2024-04-11 23:56:41 +0200

    added icons for testing and tvs

---
+++ src/icons/Azure.tsx
@@ -0,0 +1,32 @@
+import React, { SVGProps } from "react";
+
+export const AzureIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="256"
+    height="256"
+    viewBox="0 0 256 256"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M256 47.9451V203.593L192 256L92.8 219.886V255.689L36.6446 182.309L200.32 195.109V54.9486L256 47.9451ZM201.435 55.7714L109.605 0V36.5897L25.2709 61.3669L0 93.8057V167.479L36.1691 183.442V89.0331L201.435 55.7714Z"
+      fill="url(#paint0_linear_388_519)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_388_519"
+        x1="128"
+        y1="255.195"
+        x2="128"
+        y2="0.475425"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#0078D4" />
+        <stop offset="0.16" stopColor="#1380DA" />
+        <stop offset="0.53" stopColor="#3C91E5" />
+        <stop offset="0.82" stopColor="#559CEC" />
+        <stop offset="1" stopColor="#5EA0EF" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
