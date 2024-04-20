5652523e60213ecb706399858144c5c15e9aa17b
AWydra <hello@awydra.pl>
2024-04-20 19:48:06 +0200

    created section title component

---
+++ src/components/atoms/SectionTitle.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from "react";
+import cn from "classnames";
+
+interface Props extends React.HTMLAttributes<HTMLElement> {
+  children: ReactNode;
+}
+
+export const SectionTitle = ({ className, children, ...props }: Props) => (
+  <h2
+    className={cn(
+      "flex gap-3 text-[42px] font-bold leading-tight text-white before:block before:h-[1.25em] before:w-2 before:bg-secondary",
+      className,
+    )}
+    {...props}
+  >
+    {children}
+  </h2>
+);
