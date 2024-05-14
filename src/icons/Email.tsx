5f3c393ba4fc1bc4450ee2b4e842084213796f18
AWydra <hello@awydra.pl>
2024-05-14 13:32:03 +0200

    added icons for contact

---
+++ src/icons/Email.tsx
@@ -0,0 +1,16 @@
+import React, { SVGProps } from "react";
+
+export const EmailIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="256"
+    height="205"
+    viewBox="0 0 256 205"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M230.4 0H25.6C11.52 0 0.128 11.52 0.128 25.6L0 179.2C0 193.28 11.52 204.8 25.6 204.8H230.4C244.48 204.8 256 193.28 256 179.2V25.6C256 11.52 244.48 0 230.4 0ZM230.4 51.2L128 115.2L25.6 51.2V25.6L128 89.6L230.4 25.6V51.2Z"
+      fill="#000"
+    />
+  </svg>
+);
